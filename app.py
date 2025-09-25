4eded655c0c0cb1116abe8f2c8e1a6680e48531b
Joshua Luppes <lupjos@dunwoody.edu>
2025-09-24 22:45:50 -0500

    Add new variables

--- app.py
+++ app.py
@@ -8,5 +8,7 @@ app = Flask(__name__)
 def home():
     now = datetime.now()
     current_year = now.year
+    current_day = now.day
+    current_now = now
     current_month = now.strftime("%B")
-    return render_template("index.html", year=current_year, month=current_month)
+    return render_template("index.html", now=current_now, year=current_year, month=current_month, day=current_day)
